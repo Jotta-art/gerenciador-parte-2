fd878d1d8aaf25cd468f8026fb6f900dcbd59cae
João Pedro <joao.pedro42@outlook.com.br>
2021-03-03 14:02:41 -0300

    Adicionando funcionalidade de remoção de empresas

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,13 +1,11 @@
 package br.com.alura.gerenciador.servlet;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
---
+++ src/main/java/br/com/alura/gerenciador/servlet/RemoveEmpresaServlet.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/removeEmpresa")
+public class RemoveEmpresaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String paramId = request.getParameter("id");
+        Integer id = Integer.valueOf(paramId);
+
+        System.out.println(id);
+
+        Banco banco = new Banco();
+        banco.removeEmpresa(id);
+
+        response.sendRedirect("listaEmpresas");
+    }
+}
