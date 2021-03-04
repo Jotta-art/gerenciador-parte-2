176aa6c83cf4eb30e154b3279c961f7e29c7efd2
João Pedro <joao.pedro42@outlook.com.br>
2021-03-03 23:03:22 -0300

    Adicionando funcionalidade de alterção de empresas no servlet e alterando o JSP conforme a necessidade

--- src/main/java/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -4,17 +4,34 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet(urlPatterns = "/alteraEmpresa")
 public class AlteraEmpresaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println("Alterando empresa");
+
+        String nomeEmpresa = request.getParameter("nome");
+        String paramDataEmpresa = request.getParameter("data");
         String paramId = request.getParameter("id");
         Integer id = Integer.valueOf(paramId);
 
+        Date dataAbertura = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            dataAbertura = sdf.parse(paramDataEmpresa);
+        } catch (ParseException e) {
+            throw new ServletException(e);
+        }
+
         Banco banco = new Banco();
-        Empresa empresa = banco.getEmpresas().get(id);
+        Empresa empresa = banco.buscaEmpresaPeloId(id);
+        empresa.setNome(nomeEmpresa);
+        empresa.setDataAbertura(dataAbertura);
 
         response.sendRedirect("listaEmpresas");
     }
