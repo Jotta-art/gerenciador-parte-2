bf10a13fb6e3c4014ac2e1b71507bb89691e4476
João Pedro <joao.pedro42@outlook.com.br>
2021-03-11 09:38:59 -0300

    Implementando o formulário de login

--- src/main/java/br/com/alura/gerenciador/acao/MostraEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/acao/MostraEmpresa.java
@@ -3,7 +3,6 @@ package br.com.alura.gerenciador.acao;
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
